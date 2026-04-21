55af4bf3cdf0f7211f9dc12f85ca4a5e5fec1bf9
Bernardo <teatimbenard@gmail.com>
2026-04-21 10:50:49 +0300

    The Explorer

--- tourism/views.py
+++ tourism/views.py
@@ -1,8 +1,35 @@
-from django.shortcuts import render
+from django.contrib.auth import authenticate, login, logout
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from .models import Beach
 
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST.get("username")
+        password = request.POST.get("password")
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return redirect('home')
+        else:
+            return render(request, 'login.html', {'error': 'Invalid username or password'})
+
+    return render(request, 'login.html')
+
+
+@login_required
 def home(request):
+    beaches = Beach.objects.all()
+    return render(request, 'home.html', {'beaches': beaches})
+
+
+def logout_view(request):
+    logout(request)
+    return redirect('login')
+
     beaches = Beach.objects.all()
     return render(request, 'home.html', {'beaches': beaches})
 # Create your views here.
