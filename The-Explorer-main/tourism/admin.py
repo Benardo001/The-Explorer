4ef209ba80cd2fce1dbc11e771e9efe84b2da2c3
Benardo <teatimbenard@email.com>
2026-05-08 13:08:05 +0300

    Errors fixed

---
+++ The-Explorer-main/tourism/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from django.contrib import admin
+from .models import Beach, Review
+
+admin.site.register(Beach)
+admin.site.register(Review)
+# Register your models here.
